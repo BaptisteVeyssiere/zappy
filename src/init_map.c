839f6893c3b4afdcd88532759d9bb580e9d3881c
Baptiste Veyssiere <baptiste.veyssiere@epitech.eu>
2017-06-27 17:28:16 +0200

    merge

--- src/init_map.c
+++ src/init_map.c
@@ -1,11 +1,11 @@
 /*
 ** init_map.c for Project-Master in /home/nathalie/rendu/network/PSU_2016_zappy/src
-** 
+**
 ** Made by Nathalie CAI
 ** Login   <nathalie.cai@epitech.eu>
-** 
+**
 ** Started on  Mon Jun 26 16:47:55 2017 Nathalie CAI
-** Last update Tue Jun 27 11:42:16 2017 Nathalie CAI
+** Last update Tue Jun 27 17:07:27 2017 Baptiste Veyssiere
 */
 
 #include <stdlib.h>
@@ -69,17 +69,23 @@ static t_data	*fill_tile(t_data *data)
 t_data	*init_map(t_data *data)
 {
   unsigned int  i;
+  unsigned int	j;
+  int		k;
 
-  if ((data->map = malloc(sizeof (t_items *) * data->height)) == NULL)
+  if ((data->map = malloc(sizeof(t_items *) * data->height)) == NULL)
     return (NULL);
-  bzero(data->map, sizeof (t_items*));
-  i = 0;
-  while (i < data->height)
+  i = -1;
+  while (++i < data->height)
     {
-      if ((data->map[i] = malloc(sizeof (t_items) * data->width)) == NULL)
+      if ((data->map[i] = malloc(sizeof(t_items) * data->width)) == NULL)
 	return (NULL);
-      bzero(data->map[i], sizeof (t_items));
-      i++;
+      j = -1;
+      while (++j < data->width)
+	{
+	  k = -1;
+	  while (++k < ITEMNBR)
+	    data->map[i][j].item[k] = 0;
+	}
     }
   data = fill_tile(data);
   return (data);
