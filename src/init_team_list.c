4df9f4f92e27c98d3add7e457e02ee0209631d6a
Nathalie CAI <nathalie.cai@epitech.eu>
2017-06-28 15:23:57 +0200

    fixed little bit of norme

--- src/init_team_list.c
+++ src/init_team_list.c
@@ -1,16 +1,15 @@
 /*
-** init_team_list.c for Project-Master in /home/nathalie/rendu/network/PSU_2016_zappy/src
+** init_team_list.c for Project in nathalie/rendu/network/PSU_2016_zappy/src
 ** 
 ** Made by Nathalie CAI
 ** Login   <nathalie.cai@epitech.eu>
 ** 
 ** Started on  Mon Jun 26 16:38:22 2017 Nathalie CAI
-** Last update Wed Jun 28 13:52:40 2017 Nathalie CAI
+** Last update Wed Jun 28 15:23:23 2017 Nathalie CAI
 */
 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include "server.h"
 
 static  t_data	*init_nbr_teams(t_data *data, char **tab)
@@ -34,10 +33,27 @@ static	t_data	*init_team(t_data *data, char **tab, int i, int j)
   return (data);
 }
 
-t_data	*init_team_list(t_data *data, int ac, char **av)
+static t_data	*init_team_loop(t_data *data, char **tab)
 {
   int           j;
   unsigned int  i;
+
+  i = -1;
+  j = 0;
+  while (++i < data->nbr_teams)
+    {
+      if ((data->team_list[i] = malloc(sizeof(t_team))) == NULL)
+	return (NULL);
+      bzero(data->team_list[i], sizeof(t_team));
+      if ((data = init_team(data, tab, i, j++)) == NULL)
+	return (NULL);
+    }
+  data->team_list[i] = NULL;
+  return (data);
+}
+
+t_data	*init_team_list(t_data *data, int ac, char **av)
+{
   char          **tab;
 
   if ((tab = team_tab(ac, av)) == NULL)
@@ -52,17 +68,8 @@ t_data	*init_team_list(t_data *data, int ac, char **av)
        malloc(sizeof(t_team *) * (data->nbr_teams + 1))) == NULL)
     return (NULL);
   bzero(data->team_list, sizeof(t_team *) * (data->nbr_teams + 1));
-  i = -1;
-  j = 0;
-  while (++i < data->nbr_teams)
-    {
-      if ((data->team_list[i] = malloc(sizeof(t_team))) == NULL)
-	return (NULL);
-      bzero(data->team_list[i], sizeof(t_team));
-      if ((data = init_team(data, tab, i, j++)) == NULL)
-	return (NULL);
-    }
-  data->team_list[i] = NULL;
+  if ((data = init_team_loop(data, tab)) == NULL)
+    return (NULL);
   free(tab);
   return (data);
 }
