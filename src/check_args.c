b48567125421bccf6852e132f1f3c889c6b98efd
Nathalie CAI <nathalie.cai@epitech.eu>
2017-06-29 14:52:14 +0200

    check options

--- src/check_args.c
+++ src/check_args.c
@@ -1,14 +1,15 @@
 /*
-** check_args.c for Project-Master in /home/nathalie/rendu/network/PSU_2016_zappy/src
+** check_args.c for Project-Master in nathalie/rendu/network/PSU_2016_zappy/src
 ** 
 ** Made by Nathalie CAI
 ** Login   <nathalie.cai@epitech.eu>
 ** 
 ** Started on  Wed Jun 28 12:54:55 2017 Nathalie CAI
-** Last update Wed Jun 28 15:51:40 2017 Nathalie CAI
+** Last update Thu Jun 29 14:49:42 2017 Nathalie CAI
 */
 
 #include <string.h>
+#include <stdio.h>
 #include "server.h"
 
 static int	is_there_spaces(int ac, char **av, int i)
@@ -33,6 +34,35 @@ static int	check_team_names(int ac, char **av)
   return (0);
 }
 
+static char	*check_options(int ac, char **av)
+{
+  int	i;
+  int	j;
+  int	a;
+  char	*options[] = { "-p", "-x", "-y", "-n", "-c", "-f" };
+
+  i = 0;
+  while (i < ac)
+    {
+      if (strncmp(av[i], "-", 1) == 0)
+	{
+	  j = 0;
+	  a = 0;
+	  while (j < 6 && a == 0)
+	    {
+	      if (strncmp(av[i], options[j], strlen(options[j])) != 0)
+		j++;
+	      else
+		a = 1;
+	    }
+	  if (a == 0)
+	    return (av[i]);
+	}
+      i++;
+    }
+  return (NULL);
+}
+
 int	check_args(int ac, char **av)
 {
   unsigned int	width;
@@ -40,22 +70,24 @@ int	check_args(int ac, char **av)
   unsigned int	freq;
   unsigned int	player_limit;
   unsigned int	port;
+  char		*option;
 
-  width = get_uint(ac, av, "-x");
-  height = get_uint(ac, av, "-y");
+  if ((option = check_options(ac, av)) != NULL)
+    {
+      fprintf(stderr, "%s ", option);
+      return (print_error_param("is not a known option.\n"));
+    }
   freq = get_uint(ac, av, "-f");
   player_limit = get_uint(ac, av, "-c");
-  port = get_uint(ac, av, "-p");
-  if ((width != 0 && width < 10) || width > 30)
+  if (((width = get_uint(ac, av, "-x")) != 0 && width < 10) || width > 30)
     return (print_error_param("-x should be between 10 and 30.\n"));
-  if ((height != 0 && height < 10) || height > 30)
+  if (((height = get_uint(ac, av, "-y")) != 0 && height < 10) || height > 30)
     return (print_error_param("-y should be between 10 and 30.\n"));
   if ((freq != 0 && freq < 2) || freq > 1000)
     return (print_error_param("-f should be between 2 and 1000.\n"));
   if ((player_limit != 0 && player_limit < 2) || player_limit > 1000)
     return (print_error_param("-c should be between 1 and 50.\n"));
-  if ((port != 0 && port < 1025) || port > 65535)
+  if (((port = get_uint(ac, av, "-p")) != 0 && port < 1025) || port > 65535)
     return (print_error_param("-p should be between 1025 and 65535.\n"));
   return (check_team_names(ac, av));
-  return (0);
 }
